9aaabeae0e0a63086b024d5523bd32a4eca71286
Ivanmedrado <78947685+Ivanmedrado@users.noreply.github.com>
2025-01-23 18:27:01 +0000

    Super trunfo de cidades - iniciante

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,33 @@
 //Teste larissa
 
 int main() {
+
+    char cidade[50];
+    int populacao;
+    float area;
+    float pib;
+    int pontos_turisticos;
+
+    printf("\n**SUPER TRUNFO DE CIDADES** \n\n");
+
+
+    printf("Digite o nome da cidade: \n", cidade);
+    scanf("%s: ", &cidade);
+
+    printf("Digite o valor da população da cidade: \n", populacao);
+    scanf("%d: ", &populacao);
+
+    printf("Digite o valor da área da cidade: \n", area);
+    scanf("%f", &area);
+
+    printf("Digite o valor do PIB da cidade: \n", pib);
+    scanf("%f", &pib);
+
+    printf("Digite a quantidade de pontos turisticos da cidade: \n", pontos_turisticos);
+    scanf("%d", &pontos_turisticos);
+
+    printf("Os atributos da sua carta são: \n");
+    printf("Nome da cidade: %s - populacao: %d - Area: %f - PIB: %f - Pontos Turisticos: %d \n", cidade,populacao, area, pib, pontos_turisticos );
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
@@ -19,4 +46,4 @@ int main() {
     // Exiba os valores inseridos para cada atributo da cidade, um por linha.
 
     return 0;
-}
+    }
